e956eef76a4cada78bb6aa830efd02009e8ca54c
kyohei8 <tsukuda.kyouhei@gmail.com>
2017-03-27 12:37:06 +0900

    add static page system and compression.

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const graphql = require('graphql');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 const schema = require('./schema.js');
-
 const app  = express();
 const PORT = 3000;
 
+app.set('view engine', 'pug');
+app.use(express.static('assets'));
+app.use(compression());
+
 // parse POST body as text
 app.use(bodyParser.text({ type: 'application/graphql' }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.post('/graphql', (req, res) => {
   // execute GraphQL!
